cce63f110189e0bf46267c8e7dc8faf77d0028a6
Can YILMAZ <canyilmaz999@gmail.com>
2021-09-24 10:13:01 +0300

    SessionController Created

---
+++ BikeSharingAPI/Controllers/SessionController.cs
@@ -0,0 +1,65 @@
+﻿using BikeSharingAPI.Models.DTOs.Sessions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeSharingAPI.Controllers
+{
+    [Route("api/v1/Sessions")]
+    [ApiController]
+    public class SessionController : ControllerBase
+    {
+        public SessionController()
+        {
+
+        }
+
+        /// <summary>
+        /// Tum sessionlarin bir listesini doner.
+        /// </summary>
+        /// <returns>Json formatinda kullanici listesi.</returns>
+        [HttpGet]
+        public IActionResult GetSessionList()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Id'si verilen sessioni doner.
+        /// </summary>
+        /// <param name="id">Kullanici id'si</param>
+        /// <returns>Eger bulursa, session bilgileri json formatinda.</returns>
+        [Route("{id}")]
+        [HttpGet]
+        public IActionResult GetSession([FromRoute] int id)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Id ile eşleştirdiği kaydi verilen degerler ile veritabanında gunceller. Degeri belirtilmeyen
+        /// alanlara o alanlarin default degeri atanir.
+        /// </summary>
+        /// <param name="sessionUpdateDTO"></param>
+        /// <returns>if succeeds 204; if fails 400 or 500</returns>
+        [HttpPut]
+        public IActionResult PutSession([FromBody]SessionUpdateDTO sessionUpdateDTO)
+        {
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Id ile eşleştirdiği kaydi verilen degerler ile veritabanında gunceller.
+        /// </summary>
+        /// <param name="sessionUpdateDTO"></param>
+        /// <returns>if succeeds 204; if fails 400 or 500</returns>
+        [HttpPatch]
+        public IActionResult PatchSession([FromBody] SessionUpdateDTO sessionUpdateDTO)
+        {
+            return NoContent();
+        }
+    }
+}
