8be59eac9061bb24c00d8fa5d879aec1377af5ad
Can YILMAZ <canyilmaz999@gmail.com>
2021-10-02 23:52:02 +0300

    HelperResponse created

---
+++ BikeSharingAPI/Helpers/HelperXML.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace BikeSharingAPI.Helpers
+{
+    public static class HelperXML
+    {
+        public static string TOXML<T>(T obj)
+        {
+            try
+            {
+                var xml = "";
+
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                using (var sww = new StringWriter())
+                {
+                    using (XmlWriter writer = XmlWriter.Create(sww))
+                    {
+                        serializer.Serialize(writer, obj);
+                        xml = sww.ToString();
+                    }
+                }
+
+                return xml;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
