7a1ee47907a45009be78d73ba91e2cb7a922d4cf
Can YILMAZ <canyilmaz999@gmail.com>
2021-10-03 04:31:50 +0300

    Moq added to the project

---
+++ BikeSharingAPI.Test/UserServiceGetUserTest.cs
@@ -0,0 +1,18 @@
+﻿using BikeSharingAPI.Services.IServices;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeSharingAPI.Test
+{
+    public class UserServiceGetUserTest
+    {
+        public void GetUserTest()
+        {
+            var mock = new Mock<IUserService>();
+        }
+    }
+}
